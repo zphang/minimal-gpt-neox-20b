7a8156dce92be52c10de37162bc36dc5a1828093
Jason Phang <jasonphang@nyu.edu>
2022-04-05 00:53:06 -0400

    imports

--- minimal20b/__init__.py
+++ minimal20b/__init__.py
@@ -1,3 +1,4 @@
-from .model import NeoX20BModel
-from .create_model import create_model, create_dummy_model, create_tokenizer
+from .model import NeoX20BModel, generate_mask
+from .constants import Args20b, ArgsDummy
+from .create import create_model, create_dummy_model, create_tokenizer
 from .generate import greedy_generate, greedy_generate_text
